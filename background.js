5e058b4a0c8b7b25908a65605899d876c04bccea
takecare <rui@vazteixeira.org>
2022-08-03 21:13:52 +0100

    cleanup

--- background.js
+++ background.js
@@ -1,27 +1,11 @@
 /* global chrome */
 
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/cookies/Cookie
-const logCookies = (details) => {
-  browser.cookies.getAll({})
-    .then((cookies) => {
-      for (c of cookies) {
-        console.log(`> COOKIE name="${c.name}", domain="${c.domain}", value="${c.value}"`)
-      }
-    })
-}
-
 const removeCookiesOnLoadCompleted = (details) => {
-  // console.log(details)
-  // logCookies()
-  // browser.cookies.remove({name: "uid", url: details.url})
   browser.cookies.remove({name: "uid", url: details.url})
-    .then(
-      (cookie) => {
-        console.log(`> REMOVED COOKIE ON ${details.url}`, cookie);
-      }
-    )
+    .then()
     .catch(
-      (error) => console.error("> ERROR: " + error)
+      (error) => console.error("Failed to remove 'uid' cookie:", error)
     )
 }
 
@@ -32,9 +16,6 @@ const addRefererHeader = (details) => {
   const headers = details.requestHeaders;
   const referer = headers.find((header) => header.name.toLowerCase() === 'referer')
     || { name: 'Referer', value: 'https://t.co/xyZXYz12pt' };
-
-  // const newHeaders = headers.filter((header) => header.name.toLowerCase() !== 'referer');
-  // newHeaders.push({ name: 'Referer', value: 'https://t.co/xyZXYz12pt' });
   headers.push(referer);
   return { requestHeaders: headers };
 };
